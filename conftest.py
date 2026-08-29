5828eae7a7bb5999b45e9c40ca989b79656cca91
Dmitry Varavva <d-varavva@mail.ru>
2026-08-29 12:15:48 +0300

    minor modification conftest and constants

--- conftest.py
+++ conftest.py
@@ -2,7 +2,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
-from constants import LOGIN_URL
+from constants import LOGIN_URL, LOGIN_MAIL, LOGIN_PASSWORD
 from locators import Locators
 
 @pytest.fixture
@@ -16,8 +16,8 @@ def driver():
 def driver_log(driver):
     driver.get(LOGIN_URL)
     
-    driver.find_element(*Locators.EMAIL_INPUT).send_keys("дмитрий_варавва_53_000@mail.ru")
-    driver.find_element(*Locators.PASSWORD_INPUT).send_keys("76543210")
+    driver.find_element(*Locators.EMAIL_INPUT).send_keys(LOGIN_MAIL)
+    driver.find_element(*Locators.PASSWORD_INPUT).send_keys(LOGIN_PASSWORD)
     driver.find_element(*Locators.LOGIN_BUTTON).click()
 
     WebDriverWait(driver, 10).until(expected_conditions.visibility_of_element_located(Locators.MAIN_PAGE_TITLE))
--- constants.py
+++ constants.py
@@ -4,3 +4,6 @@ REGISTER_URL = f'{MAIN_URL}register'
 LOGIN_URL = f'{MAIN_URL}login'
 FORGOT_PASSWORD_URL = f'{MAIN_URL}forgot-password'
 PROFILE_URL = f'{MAIN_URL}account/profile'
+
+LOGIN_MAIL = "дмитрий_варавва_53_000@mail.ru"
+LOGIN_PASSWORD = "76543210"
